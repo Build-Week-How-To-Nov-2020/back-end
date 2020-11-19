4a025c2c0edb147b4298fbcaf4c2f5edceb5a3e3
Christopher W Schultz <chris.william.schultz@gmail.com>
2020-11-19 16:29:51 -0700

    added try catch

--- data/models/stepModel.js
+++ data/models/stepModel.js
@@ -1,24 +1,44 @@
 const db = require('../config')
 
 async function getStep(stepId) {
-    return db('steps').where('id', stepId)
+    try {
+        return db('steps').where('id', stepId)
+    }
+    catch (error) {
+        return error
+    }
 }
 
 async function getStepsForGuide(guideId) {
-    return db('steps').where('guideId', '=', guideId)
+    try {
+        return db('steps').where('guideId', '=', guideId)
+    }
+    catch (error) {
+        return error
+    }
 }
 
 async function createStep(data, guideId) {
-    if (typeof guideId !== "undefined") {
-        let [id] = await db('steps').insert({title: data.title, instruction: data.instruction, guideId: guideId})
-        return id
-    } else {
-        return -1
+    try {
+        if (typeof guideId !== "undefined") {
+            let [id] = await db('steps').insert({title: data.title, instruction: data.instruction, guideId: guideId})
+            return id
+        } else {
+            return -1
+        }
+    }
+    catch (error) {
+        return error
     }
 }
 
 async function updateStep(stepId, data) {
-    return db('steps').where('id', stepId).update({title: data.title, instruction: data.instruction})
+    try {
+        return db('steps').where('id', stepId).update({title: data.title, instruction: data.instruction})
+    }
+    catch (error) {
+        return error
+    }
 }
 
 async function deleteStep(stepId) {
